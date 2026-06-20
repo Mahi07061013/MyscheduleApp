44b90a706592442be49b5cd4723dcc4629890b33
vacuum11@icloud.com <vacuum11@icloud.com>
2026-06-20 12:53:43 +0900

    Initial Commit

---
+++ MyscheduleApp/MyscheduleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyscheduleAppApp.swift
+//  MyscheduleApp
+//
+//  Created by Kato Mahiro on 2026/06/15.
+//
+
+import SwiftUI
+
+@main
+struct MyscheduleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyscheduleAppTests/MyscheduleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyscheduleAppTests.swift
+//  MyscheduleAppTests
+//
+//  Created by Kato Mahiro on 2026/06/15.
+//
+
+import Testing
+@testable import MyscheduleApp
+
+struct MyscheduleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
